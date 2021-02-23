2e6c83af644b1e2ed96aa3c22c2850b801287deb
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-22 21:28:57 -0800

    encapsulate more

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -14,7 +15,24 @@ namespace GradeBook
       {
         grades.Add(grade);
       }
+    public void ShowStatistics()
+    {
+       var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach (double number in grades)
+            {   
+                highGrade = Math.Max(number, highGrade);   
+                lowGrade = Math.Min(number, lowGrade);
+                result += number;
+            }
+            result /= grades.Count;
+        Console.WriteLine($"The average grade is {result:N1}");
+        Console.WriteLine($"The Highest grade is {highGrade:N1}");
+        Console.WriteLine($"The Lowest grade is {lowGrade:N1}");
+    }
       private List<double> grades;
       private string name;
-    }
+
+  }
 }
